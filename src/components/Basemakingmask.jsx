3a0d5376edc96dffd40af4366ae13655db2d69a9
ooohmikey <ooohmikey@yahoo.co.jp>
2020-06-08 19:55:16 +0900

    add messages

--- src/components/Basemakingmask.jsx
+++ src/components/Basemakingmask.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const basemakingmask = props => {
     return (
         <div>
-            <p>basemakingmask</p>
+            <a href="https://www.p-dc.com/shop/g/g4961989107981/">リフターナ　ベースメイキングマスク　白肌</a>
+            <p>透明感UPは必須です。</p>
+            <p>35才過ぎてから顔のトーンが落ちてくるもんね。</p>
+            <p>目指すは韓国ドラマの女優さん！</p>
+            <p>これの凄いところは</p>
+            <p>"スキンケア効果で毛穴肌をキュッ！とひきしめ、フラットパウダーで、でこぼこ毛穴を隠す、同時のＷケア！"</p>
+            <p>５枚も入ってるのでやり放題です。</p>            
         </div>
     );
 }
--- src/components/Keanaricemask.jsx
+++ src/components/Keanaricemask.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 const keanaricemask = props => {
     return (
         <div>
-            <p>Ricemask</p>
+        <div>
+            <a href="https://www.ishizawa-lab.co.jp/keananadeshiko/product/keana_skincare/mask.html">毛穴撫子 お米のマスク</a>
+            <p>毛穴ケアで検索したらこれがトップに出ました。</p>
+            <p>ベストコスメアワードで第一位</p>
+            <p>2018年だけど。</p>
+            <p>2019年の分は見当たらなかったっす。</p>
+            <p>それはさておき、</p>
+            <p>厚手で10枚も入ってるふとっぱらプロダクト</p>
+            <p>やり放題ですね。</p>            
+            <p>これも毛穴がキュッとなるの期待できそうです。</p>
+            <p>せっせと頑張っていきましょう。</p>
+        </div>
         </div>
     );
 }
