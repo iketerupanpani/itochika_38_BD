2aaf8196cd3e7d042cd7142754e81a7cba35422b
ooohmikey <ooohmikey@yahoo.co.jp>
2020-06-13 15:23:34 +0900

    menu change

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
-import React from 'react';
-import Message from './components/Message';
-import Ion from './components/Ionstick';
-import Rice from './components/Keanaricemask';
-import Base from './components/Basemakingmask';
-import Medi from './components/Medianswer';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import React from "react";
+import Message from "./components/Message";
+import Ion from "./components/Ionstick";
+import Rice from "./components/Keanaricemask";
+import Base from "./components/Basemakingmask";
+import Medi from "./components/Medianswer";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
@@ -13,21 +13,33 @@ const App = () => {
       <div>
         <h1>　🎂　🎉　❤️</h1>
         <ul>
-          <li><Link to='/'>メッセージ</Link></li>
-          <li><Link to='/ionstick'>プレゼント１</Link></li>
-          <li><Link to='/keanarichmask'>プレゼント２</Link></li>
-          <li><Link to='/basemakingmask'>プレゼント３</Link></li>
-          <li><Link to='/medianswer'>プレゼント４</Link></li>
+          <li>
+            <Link to="/">誕生日メッセージ</Link>
+          </li>
+          <li>
+            <Link to="/ionstick">Ion Stick</Link>
+          </li>
+          <li>
+            <Link to="/keanarichmask">毛穴撫子</Link>
+          </li>
+          <li>
+            <Link to="/basemakingmask">
+              リフターナ　ベースメイキングマスク　白肌
+            </Link>
+          </li>
+          <li>
+            <Link to="/medianswer">Real Skin Fit Collagen Mask -ABOUT ME-</Link>
+          </li>
         </ul>
-        <hr/>
-        <Route exact path='/' component={Message} />
-        <Route path='/ionstick' component={Ion} />
-        <Route path='/keanarichmask' component={Rice}/>
-        <Route path='/basemakingmask' component={Base} />
-        <Route path='/medianswer' component={Medi} />
+        <hr />
+        <Route exact path="/" component={Message} />
+        <Route path="/ionstick" component={Ion} />
+        <Route path="/keanarichmask" component={Rice} />
+        <Route path="/basemakingmask" component={Base} />
+        <Route path="/medianswer" component={Medi} />
       </div>
     </BrowserRouter>
-    );
-}
+  );
+};
 
 export default App;
